8b6a83165470a90f753c9e73d89f9cb8edc231c6
Rick Mastry <jahrik@hotmail.com>
2022-03-27 16:20:45 -0400

    initial coomit

--- js/main.js
+++ js/main.js
@@ -32,3 +32,16 @@ function toggleBtn(event){
     showMenu = false;
 }
 
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.querySelector(".navbar").style.padding = "25px 10px";
+    document.querySelector(".navbar-brand").style.width = "70px";
+    document.querySelector(".navbar-brand").style.height = "70px";
+  } else {
+    document.querySelector(".navbar").style.padding = "70px 10px";
+    document.querySelector(".navbar-brand").style.width = "90px";
+    document.querySelector(".navbar-brand").style.height = "90px";
+  }
+}
+
